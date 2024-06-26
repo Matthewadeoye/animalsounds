5797929526e4301e604b0551cb750061d26ff844
Matthew Adeoye <95089879+Matthewadeoye@users.noreply.github.com>
2024-06-26 13:22:19 +0100

    commit

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,14 @@
+#' An R package to describe what an animal does
+#'
+#'Prints what the animal does *(the sound it makes)* in the console, [paste0()].It doesn't use [basemodels::dummy_classifier()]
+#'
+#' @param animal The name of the animal in strings
+#' @param sound  The sound produced by the animal in strings
+#'
+#' @return Print the full sentence
+#' @export
+#'
+#' @examples
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
